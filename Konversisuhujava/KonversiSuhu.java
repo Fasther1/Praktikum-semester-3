db29190e6c127d9c54ae24717404dab49d999dd0
Fakhri Rifqi Fahreza <114640280+Fasther1@users.noreply.github.com>
2023-10-25 22:06:10 +0700

    Add files via upload

--- Konversisuhujava/KonversiSuhu.java
+++ Konversisuhujava/KonversiSuhu.java
@@ -1,10 +1,10 @@
 class KonversiSuhu {
-    // Method konversi dari Celsius ke Fahrenheit
+    
     public double celciusToFahrenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
     }
     
-    // Method konversi dari Celsius ke Reamur
+    
     public double celciusToReamur(double celsius) {
         return celsius * 4 / 5;
     }
--- Konversisuhujava/KonversiSuhu2.java
+++ Konversisuhujava/KonversiSuhu2.java
@@ -1,7 +1,7 @@
 class KonversiSuhu2 extends KonversiSuhu {
-    // Method konversi dari Fahrenheit ke Reamur
+    
     public double fahrenheitToReamur(double fahrenheit) {
         double celsius = (fahrenheit - 32) * 5 / 9;
-        return celciusToReamur(celsius); // Menggunakan metode dari superclass
+        return celciusToReamur(celsius); 
     }
 }
